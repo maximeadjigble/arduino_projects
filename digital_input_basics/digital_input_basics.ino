5a89692920540976ab1773874a28c7f82e15bc6c
Maxime Adjigble <maxime.adjigble@gmail.com>
2019-05-01 18:19:50 +0100

    Removed Blink

--- digital_input_basics/digital_input_basics.ino
+++ digital_input_basics/digital_input_basics.ino
@@ -5,17 +5,15 @@ int inputPinState_prevCycle = 1;
 int ledState = LOW;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(inputPin, INPUT);
   pinMode(ledPin, OUTPUT);
   
   //Serial.begin(9600);
-  
   //while(!Serial){}
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   inputPinState = digitalRead(inputPin);
 
   if(inputPinState > inputPinState_prevCycle){
